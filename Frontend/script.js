09ccda5e530881a19df47c0263a6cd09c76dfe42
AbsoluteCrest <piotrek.ruzanski@gmail.com>
2024-02-14 22:27:04 +0100

    nudy

---
+++ Frontend/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#hide").click(function () {
+        $("#divimg1").hide();
+    });
+
+    $("#show").click(function(){
+        $("#divimg1").show();
+    });
+});
